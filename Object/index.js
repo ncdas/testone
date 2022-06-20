4d7bb2266a7abcd691ba42cdb9adcd04e7a86a0f
NC Das <nimai.iyf@gmail.com>
2022-06-20 08:20:16 +0600

    commit plus operator

---
+++ Object/index.js
@@ -0,0 +1,8 @@
+
+const shape = {
+    radius: 10,
+    diameter(){
+        return this.radius * 2;
+    },
+};
+console.log(shape.diameter());
---
+++ w3sl/assignment.js
@@ -0,0 +1,5 @@
+let x, y;
+x = 10;
+y = 5;
+x = x + y;
+console.log(x);
